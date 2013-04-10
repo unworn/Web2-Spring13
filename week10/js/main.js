b149eba759020291b0e8aa3910e96a2c106180ce
Nidhi <nidhi.m@gmail.com>
2013-04-09 20:31:42 -0400

    not much changed

--- week10/js/main.js
+++ week10/js/main.js
@@ -2,6 +2,8 @@ var d = new Date();
 var h = d.getHours();
 var m = d.getMinutes();
 
+/* console.log("this is a console log statement " + h); */
+
 function showTime(){
 	if(h <= 12 && h >= 5){
 		console.log("Good morning! It's "+h +":" + m + "am");
@@ -29,14 +31,15 @@ $(document).ready(function() {
 		$(this).empty();
 		$(this).append("The time is");
 		
-	$("p#time").empty();
-  		/* $("p#time").append(h + ":" + m); */
+		$("p#time").empty();
+  		$("p#time").append(h + ":" + m);
   		$("#wrapper").css("background-color","#999");
 	});
 	
-	$("p#time").append(h + ":" + m);
+	
 	
 });
+
 
 /*
 
